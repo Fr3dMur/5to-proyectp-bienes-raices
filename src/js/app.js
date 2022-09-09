b8ba3a58c74ee94f7cdb9c3d890777c24aa382db
Fr3dMur <murillo.freddy2002@gmail.com>
2022-09-09 17:08:33 +0200

    updating configuration for  darkmode

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,23 @@ document.addEventListener('DOMContentLoaded',function(){
 
 function modeDark() {
 
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode')
+    } else {
+        document.body.classList.remove('dark-mode')
+    }
+
+    prefiereDarkMode.addEventListener('change', function(){
+        
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode')
+    } else {
+        document.body.classList.remove('dark-mode')
+    }
+    })
+
     const darkModeBoton = document.querySelector('.dark-mode-boton');
 
     darkModeBoton.addEventListener('click', function(){
